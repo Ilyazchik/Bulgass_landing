355621fd4a392122103349cd45f01ac986ac398b
bulgass <bswcourses@gmail.com>
2024-12-11 21:09:23 +0600

    change href in inst and wats

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'https://cdn.skypack.dev/react-router-dom@5.3.0';
 
 
 function Footer() {
@@ -6,30 +5,30 @@ function Footer() {
     <footer className="flex justify-center items-center bottom-0 left-0 right-0 bg-white py-4">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 w-full flex justify-between items-center">
         
-        {/* Social links */}
+        
         <ul className="flex mb-4">
           <li className="ml-4">
-            <Link 
-              to="https://www.instagram.com/bulgass.it"
+            <a 
+              href="https://www.instagram.com/bulgass.it"
               className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" 
               aria-label="Instagram"
               target="_blank"
               rel="noopener noreferrer"
             >
               <i className="fab fa-instagram text-3xl"></i>
-            </Link>
+            </a>
           </li>
 
           <li className="ml-4">
-            <Link 
-              to="https://wa.me/996508522960" 
+            <a 
+              href="https://wa.me/996508522960" 
               className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" 
               aria-label="WhatsApp"
               target="_blank"
               rel="noopener noreferrer"
             >
               <i className="fab fa-whatsapp text-3xl"></i>
-            </Link>
+            </a>
           </li>
 
         </ul>
